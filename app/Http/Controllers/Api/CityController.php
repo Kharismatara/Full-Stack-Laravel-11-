0c1938d306cee951241f5a1c5da1d6daa39b5e7b
kharismatara <kharismataram@gmail.com>
2024-12-05 10:44:55 +0700

    city

--- app/Http/Controllers/Api/CityController.php
+++ app/Http/Controllers/Api/CityController.php
@@ -2,10 +2,21 @@
 
 namespace App\Http\Controllers\Api;
 
+use App\Models\City;
 use App\Http\Controllers\Controller;
+use App\Http\Resources\Api\CityResource;
 use Illuminate\Http\Request;
 
 class CityController extends Controller
 {
-    //
+    public function index(){
+        $cities = City::withCount('officeSpace')->get();
+        return CityResource::collection($cities);
+    }
+
+    public function show(City $city){
+        $city->load(['officeSpace.city', 'officeSpace.photos']);
+        $city->loadCount('officeSpaces');
+        return new CityResource($city);
+    }
 }
