1bb530ba58f0a61c2bc9354093b4b4bf9c6eea2a
kharismatara <kharismataram@gmail.com>
2024-12-07 13:27:42 +0700

    ofice controller

--- app/Http/Controllers/Api/OfficeSpaceController.php
+++ app/Http/Controllers/Api/OfficeSpaceController.php
@@ -2,10 +2,21 @@
 
 namespace App\Http\Controllers\Api;
 
+use App\Filament\Resources\OfficeSpaceResource;
 use App\Http\Controllers\Controller;
+use App\Http\Resources\Api\OfficeSpaceResource as ApiOfficeSpaceResource;
+use App\Models\OfficeSpace;
 use Illuminate\Http\Request;
 
 class OfficeSpaceController extends Controller
 {
-    //
+    public function index(){
+        $officeSpaces = OfficeSpace::with(['city'])->get();
+        return ApiOfficeSpaceResource::collection($officeSpaces);
+    }
+    public function show(OfficeSpace $officeSpace){
+        $officeSpace->load(['city', 'photos', 'benefits']);
+        return new ApiOfficeSpaceResource($officeSpace);
+    }
+
 }
